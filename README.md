85a35f6bd2efed6f0b58053e53e1288232a9e4bd
David LePage <dwlepage70@gmail.com>
2017-05-29 18:51:00 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,155 @@
-# stonesoft-azure
-Stonesoft Deployer for Azure
+### Stonesoft Azure Deployer
+
+Features:
+
+ * Auto deploy and provision Stonesoft FW in Azure and SMC
+ * Provisioning from on box templates
+ * Provisioning from remote template locations
+
+#### Installation
+
+1. Setup virtualenv:
+
+    ```
+    virtualenv -p /usr/local/bin/python3.5 venv351
+    . venv/bin/activate
+    ```
+
+2. Clone the repository and install:
+
+    ```
+    git clone https://github.com/gabstopper/stonesoft-azure.git
+    cd stonesoft-azure
+    ```
+    
+    Install all required libraries within the virtual environment:
+
+    ```
+    pip install -r requirements.txt
+    ```
+
+3. Create environment variables with the necessary IDs for Azure authentication. You can use either ServiceCredentials or UserCredentials.
+	
+	If using ServiceCredentials, set the following environment variables:
+
+	```
+	export AZURE_TENANT_ID={your Azure Active Directory tenant id or domain}
+	export AZURE_CLIENT_ID={your Azure Active Directory Application Client ID}
+	export AZURE_CLIENT_SECRET={your Azure Active Directory Application Secret}
+	```
+	
+	If using UserCredentials, set the following environment variables:
+	
+	```
+	export AZURE_USERNAME={my_username}
+	export AZURE_PASSWORD={my_password}
+	export AZURE_SUBSCRIPTION_ID={subscription_id}
+	```
+	
+	ServiceCredentials are tried first, then fall back to UserCredentials.
+	
+	For more information on Azure authentication, see: [Resource Management Authentication](http://azure-sdk-for-python.readthedocs.io/en/latest/quickstart_authentication.html#).
+	
+4. Set Stonesoft SMC credential environment variables:
+
+    The deployer requires smc-python to interface with the SMC in order to automate the layer 3 firewall creation. There are multiple ways to provide credentials for smc-python, for more information, see smc-python: [creating the session](http://smc-python.readthedocs.io/en/latest/pages/session.html).
+
+    For simplicity, set the following environment variables for the SMC server and client API Key:
+
+    ```
+    export SMC_API_KEY={key_generated_for_my_api_client}
+    export SMC_ADDRESS={http://10.10.10.10}
+    ```
+
+5. Generate your public SSH key:
+
+    Your machine will need an SSH public key when creating the deployment to enable remote access to the NGFW and clients. The script will look for the public key in ``~/.ssh/id_rsa.pub``. This key will be used in the parameters template to deploy to the virtual machine/s.
+
+    See Microsoft Azure documentation with instructions on creating your own key: https://docs.microsoft.com/en-us/azure/virtual-machines/linux/mac-create-ssh-keys?toc=%2fazure%2fvirtual-machines%2flinux%2ftoc.json
+
+    For Mac/Linux it's as easy as:
+
+    ```
+    ssh-keygen -t rsa -b 2048
+    chmod 400 ~/.ssh/id_rsa.pub
+    ```
+
+#### Running the deployer
+
+To run the deployer, execute stonesoft-azure.py.
+
+Top level commands specify the action (list, create, destroy). Each top level command may have required
+switches as well. Use the help option to display the menus.
+
+Example commands for creating:
+
+```
+(venv351) $ python stonesoft_azure.py create -h
+usage: stonesoft_azure.py create [-h]
+                                 (--template_path [TEMPLATE_PATH] | --template_link [TEMPLATE_LINK])
+                                 --engine_username ENGINE_USERNAME
+                                 --resource_group RESOURCE_GROUP
+                                 [--location_id [LOCATION_ID]]
+                                 [--deployment_name [DEPLOYMENT_NAME]]
+                                 [--tag_value [TAG_VALUE]]
+                                 [--engine_policy [ENGINE_POLICY]]
+                                 [--engine_location [ENGINE_LOCATION]]
+                                 [--force_remove]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --template_path [TEMPLATE_PATH]
+                        Path to Azure template
+  --template_link [TEMPLATE_LINK]
+                        URL of Azure template
+  --location_id [LOCATION_ID]
+                        Azure Location id
+  --deployment_name [DEPLOYMENT_NAME]
+                        Azure deployment name
+  --tag_value [TAG_VALUE]
+                        Azure tag value for the NGFW instance
+  --engine_policy [ENGINE_POLICY]
+                        Engine policy to assign
+  --engine_location [ENGINE_LOCATION]
+                        Engine location (if SMC is behind NAT)
+  --force_remove        Force remove NGFW if it exists
+
+Required template parameters:
+  --engine_username ENGINE_USERNAME
+                        Engine username for SSH access
+  --resource_group RESOURCE_GROUP
+                        Azure resource group
+           
+```
+
+You must provide either ``--template_path`` or ``--template_link``. In addition, there are other optional settings available to customize the NGFW configuration within the SMC, such as setting an engine location, (if SMC is behind NAT), policy to assign, 
+
+##### Deploy using template on local system:
+
+If you cloned the repository, you will find common templates in the /templates directory. To use these, or other ones on your local system, use ``--template_path`` when calling the script:
+
+```
+(venv351) python stonesoft_azure.py \
+    --engine_username dlepage \
+    --resource_group dlepage-rg \
+    --engine_location='Internet' \
+    --template_path=templates/engine.json \
+    --location_id westeurope \
+    --force_remove
+```
+
+##### Deploy using template on remote site:
+
+You can also deploy templates that are located in a remote location accessible by http or https. To use these, use ``--template_link`` when calling the script:
+
+```
+(venv351) python stonesoft_azure.py \
+    --engine_username dlepage \
+    --resource_group dlepage-rg \
+    --engine_location='Internet' \
+    --template_link=https://raw.githubusercontent.com/azuredeploy.json \
+    --location_id westeurope \
+    --force_remove
+```
+
+There are a variety of templates that can be used to meet common use cases stored in the templates/ directory. These are also cross linked to the Azure Portal.
---
+++ stonesoft_azure.py
@@ -0,0 +1,327 @@
+"""
+Fully provision a Stonesoft NGFW into Microsoft Azure.
+
+The NGFW is created within a resource group which will be created with a 
+custom tag of 'stonesoft' so it can be searched after by using the ``list``
+argument.
+"""
+import os
+import json
+import logging
+import argparse
+from azure.common.credentials import UserPassCredentials, ServicePrincipalCredentials
+from azure.mgmt.resource.resources import ResourceManagementClient
+from azure.mgmt.resource.resources.models import DeploymentMode
+from smc import session
+from smc.core.engines import Layer3Firewall
+from smc.elements.helpers import location_helper
+from smc.elements.network import Network
+from msrestazure.azure_exceptions import CloudError
+from smc.policy.layer3 import FirewallPolicy
+from smc.api.exceptions import ElementNotFound
+from azure.mgmt.resource.resources.v2017_05_10.models.template_link import TemplateLink
+from azure.mgmt.resource.subscriptions.v2016_06_01.subscription_client import SubscriptionClient
+
+logger = logging.getLogger(__name__)
+    
+# This script expects that the following settings are in place:
+
+# If using Service Credentials:
+# AZURE_TENANT_ID: with your Azure Active Directory tenant id or domain
+# AZURE_CLIENT_ID: with your Azure Active Directory Application Client ID
+# AZURE_CLIENT_SECRET: with your Azure Active Directory Application Secret
+
+# If using user credentials:            
+# export AZURE_USERNAME={my_username}
+# export AZURE_PASSWORD={my_password}
+# export AZURE_SUBSCRIPTION_ID={subscription_id}
+
+# Note: Service credentials are attempted first.
+
+# For the SMC Credentials, set the following environment variables:
+# export SMC_API_KEY={key_generated_for_my_api_client}
+# export SMC_ADDRESS={http://10.10.10.10}
+
+# You have an SSH public key stored in '~/.ssh/id_rsa.pub'
+
+# Deploy from a local template:
+# (venv351) python stonesoft_azure.py create \
+#    --engine_username dlepage \
+#    --resource_group dlepage-rg \
+#    --engine_location='Internet' \
+#    --template_path=templates/engine.json \
+#    --location_id westeurope \
+#    --force_remove
+
+# Deploy from a github based template with extra options:
+# (venv351) python stonesoft_azure.py create \
+#    --engine_username dlepage \
+#    --resource_group dlepage-rg \
+#    --engine_location='Internet' \
+#    --template_link=https://raw.githubusercontent.com/gabstopper/stonesoft-cloud/master/azuredeploy.json \
+#    --location_id westeurope \
+#    --tag_value 'my custom value'
+#    --force_remove
+
+# List examples
+# python stonesoft_azure.py list --by_tag stonesoft
+# python stonesoft_azure.py list --all
+# python stonesoft_azure.py list --resources_by_group {myresourcegroup}
+
+
+subscription_id = os.environ.get('AZURE_SUBSCRIPTION_ID', '') 
+
+
+def provision_stonesoft(name, vnet=None, location=None):
+    """
+    Create stonesoft firewall
+    """
+    engine = Layer3Firewall.create_dynamic(
+        name=name,
+        interface_id=0,
+        dynamic_index=1,
+        default_nat=False,
+        location_ref=location_helper(location))
+    
+    # License and Save Initial Configuration
+    node = engine.nodes[0]
+    node.bind_license()
+    return node.initial_contact(as_base64=True)
+    
+    
+def provision_stonesoft_policy():
+    pass
+
+
+def azure_credentials():
+    
+    try:  # ServicePrincipalCredentials first
+        credentials = ServicePrincipalCredentials(
+            client_id=os.environ['AZURE_CLIENT_ID'],
+            secret=os.environ['AZURE_CLIENT_SECRET'],
+            tenant=os.environ['AZURE_TENANT_ID'])
+    except KeyError:
+        credentials = UserPassCredentials(
+            username=os.environ['AZURE_USERNAME'],
+            password=os.environ['AZURE_PASSWORD'])
+    
+    return credentials
+
+
+def client():
+    
+    credentials = azure_credentials()
+
+    return ResourceManagementClient(
+        credentials,
+        subscription_id)
+    
+
+def create(namespace):
+
+    if namespace.template_path:
+        with open(namespace.template_path, 'r') as template_file_fd:
+            template = json.load(template_file_fd)
+
+    pub_ssh_key_path = os.path.expanduser('~/.ssh/id_rsa.pub') # the path to rsa public key file
+    
+    with open(pub_ssh_key_path, 'r') as pub_ssh_file_fd:
+        pub_ssh_key = pub_ssh_file_fd.read()
+
+    session.login()
+    if namespace.force_remove:
+        try:
+            Layer3Firewall(namespace.resource_group).delete()
+        except ElementNotFound:
+            pass
+
+    engineCfg = provision_stonesoft(
+        name=namespace.resource_group, 
+        vnet=None,
+        location=namespace.engine_location)
+
+    parameters = {
+        'engineCfg': engineCfg,
+        'engineUsername': namespace.engine_username,
+        'sshKey':  pub_ssh_key
+    }
+    parameters = {k: {'value': v} for k, v in parameters.items()}
+    
+    resource_client = client()
+    
+    resource_group_params = {
+        'location': namespace.location_id,
+        'tags': {'stonesoft': namespace.tag_value}
+    }
+
+    try:
+        resource_client.resource_groups.create_or_update(
+            namespace.resource_group,
+            resource_group_params)
+                                               
+        deployment_properties = {
+            'mode': DeploymentMode.incremental,
+            'parameters': parameters,
+            'template': None,
+            'template_link': None
+        }
+
+        if namespace.template_path:
+            deployment_properties.update(
+                template=template)
+        else:
+            deployment_properties.update(
+                template_link=TemplateLink(namespace.template_link))
+        
+        deployment_async_operation = resource_client.deployments.create_or_update(
+            resource_group_name=namespace.resource_group,
+            deployment_name=namespace.deployment_name,
+            properties=deployment_properties
+        )   # AzureOperationPoller
+        
+        initial_result = deployment_async_operation.result(timeout=30)
+        
+        logger.info('Starting Azure deployment; correlation id: %s',
+            initial_result.properties.correlation_id)
+       
+        logger.info('Azure provisioning state: %s',
+            initial_result.properties.provisioning_state)
+
+        while not deployment_async_operation.done():
+            deployment_async_operation.wait(timeout=30)
+            
+            status = resource_client.deployments.get(
+                namespace.resource_group,
+                deployment_name=namespace.deployment_name)
+            
+            logger.info('Azure provisioning state: %s',
+                status.properties.provisioning_state)
+    
+        result = deployment_async_operation.result() 
+        
+        elapsed_time = result.properties.timestamp - initial_result.properties.timestamp
+        logger.info('Elapsed Time: %s (seconds)', elapsed_time.total_seconds())
+        
+        policy = FirewallPolicy.get_or_create(
+            name=namespace.engine_policy)
+        
+        task = policy.upload(namespace.resource_group)
+        for percentage in task.wait(timeout=5):
+            logger.info('Stonesoft NGFW Policy upload task: {}%'.format(percentage))
+
+        if not task.success:
+            logger.error(task.last_message)
+
+        for k, v in result.properties.outputs.items():
+            print('Attribute: %s, Value: %s', k, v.get('value'))
+             
+    except CloudError:
+        Layer3Firewall(
+            namespace.resource_group).delete()
+        raise
+    finally:
+        session.logout()
+
+
+def destroy(namespace):
+
+    resource_client = client()
+    if resource_client.resource_groups.check_existence(namespace.resource_group):
+        poller = resource_client.resource_groups.delete(  # AzureOperationPoller
+            namespace.resource_group)
+        session.login()
+        try:
+            Layer3Firewall(namespace.resource_group).delete()
+        except ElementNotFound:
+            pass
+        session.logout()
+        return poller.result(timeout=10)
+
+
+def list_deployed(namespace):
+
+    if namespace.all:
+        query = None
+    
+    elif namespace.by_tag:
+        query = "tagname eq '%s'" % namespace.by_tag
+    
+    elif namespace.resources_by_group:
+        pages = resource_client.resources.list_by_resource_group(
+            namespace.resources_by_group)
+        for page in pages:
+            print(page.name, page.type, page.location)
+        return
+    
+    elif namespace.all_locations:
+        subclient = SubscriptionClient(
+            azure_credentials())
+        locations = subclient.subscriptions.list_locations(subscription_id)
+        print([location.name for location in locations])
+        return
+    
+    resource_client = client()
+    pages = resource_client.resource_groups.list(filter=query)
+    for page in pages:
+        print(page.name, page.location, page.managed_by)
+
+        
+if __name__ == '__main__':
+    
+    logger = logging.getLogger()
+    formatter = logging.Formatter('%(asctime)s [%(levelname)s] - %(name)s %(message)s')
+    handler = logging.StreamHandler()
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.INFO)
+    
+    parser = argparse.ArgumentParser(description='Stonesoft Azure Deployer')
+    parser.add_argument('--quiet_logging', action='store_true', help='Disable any logging')
+    
+    subparsers = parser.add_subparsers()
+    parser_create = subparsers.add_parser('create', help='Create NGFW resources')
+    template = parser_create.add_mutually_exclusive_group(required=True)
+    template.add_argument('--template_path', nargs='?', help='Path to Azure template')
+    template.add_argument('--template_link', nargs='?', help='URL of Azure template')
+    required = parser_create.add_argument_group('Required template parameters')
+    required.add_argument('--engine_username', required=True, help='Engine username for SSH access')
+    required.add_argument('--resource_group', required=True, help='Azure resource group')
+    parser_create.add_argument('--location_id', nargs='?', default='westus', help='Azure Location id')
+    parser_create.add_argument('--deployment_name', nargs='?', default='ngfw_azure_deployment', help='Azure deployment name')
+    parser_create.add_argument('--tag_value', nargs='?', default='smc-python', help='Azure tag value for the NGFW instance')
+    parser_create.add_argument('--engine_policy', nargs='?', default='_Azure_Default', help='Engine policy to assign')
+    parser_create.add_argument('--engine_location', nargs='?', default=None, help='Engine location (if SMC is behind NAT)')
+    parser_create.add_argument('--force_remove', action='store_true', help='Force remove NGFW if it exists')
+    parser_create.set_defaults(func=create)
+    
+    parser_destroy = subparsers.add_parser('destroy', help='Remove NGFW resources')
+    parser_destroy.add_argument('-r', '--resource_group', required=True, help='Resource group to remove')
+    parser_destroy.set_defaults(func=destroy)
+    
+    parser_list = subparsers.add_parser('list', help='List NGFW resources')
+    options = parser_list.add_mutually_exclusive_group(required=True)
+    options.add_argument('-t', '--by_tag', nargs='?', const='stonesoft', help='List resources with specific tag')
+    options.add_argument('-a', '--all', action='store_true', help='List all resources regardless of tag')
+    options.add_argument('-g', '--resources_by_group', nargs='?', help='List all resources in a resource group')
+    options.add_argument('-l', '--all_locations', action='store_true', help='List all resources in a resource group')
+    parser_list.set_defaults(func=list_deployed)
+
+    namespace = parser.parse_args()
+    
+    if not any(vars(namespace).values()):
+        parser.print_help()
+        parser.exit()
+
+    if namespace.quiet_logging:
+        logger.setLevel(logging.CRITICAL)
+    
+    try:
+        c = azure_credentials()
+    except KeyError:
+        raise Exception(
+            'Unable to find the Azure credentials within the users environment. '
+            'See documentation for more info.')
+    else:
+        namespace.func(namespace)
+
+#At least one resource deployment operation failed. Please list deployment operations for details
